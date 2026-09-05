2a75ca078482b53f10b45d7111933053e68b9b98
jamesonrb2013 <jamesonrb2013@gmail.com>
2026-09-05 16:06:59 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,10 @@ export NODE_ENV=production
 
 case "$1" in
     migrate) exec pnpm run --silent run:migrate;;
-    bot) exec pnpm run --silent run:bot;;
+    bot)
+        node -e 'require("http").createServer((req,res)=>res.end("OK")).listen(process.env.PORT||10000,"0.0.0.0")' &
+        exec pnpm run --silent run:bot
+        ;;
     api) exec pnpm run --silent run:api;;
     dashboard) exec pnpm run --silent run:dashboard;;
     *)
