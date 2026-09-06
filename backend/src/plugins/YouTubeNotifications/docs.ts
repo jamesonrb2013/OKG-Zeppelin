a828d1befe868e82fb32d3ca1d257c93c1dd65bc
jamesonrb2013 <jamesonrb2013@gmail.com>
2026-09-06 16:05:01 -0600

    Add documentation for YouTube Notifications plugin

---
+++ backend/src/plugins/YouTubeNotifications/docs.ts
@@ -0,0 +1,9 @@
+import { ZeppelinPluginDocs } from "../../types.js";
+import { zYouTubeNotificationsConfig } from "./types.js";
+
+export const youtubeNotificationsPluginDocs: ZeppelinPluginDocs = {
+  prettyName: "YouTube Notifications",
+  description: "Posts notifications when a configured YouTube channel publishes a new video.",
+  type: "guild",
+  configSchema: zYouTubeNotificationsConfig,
+};
